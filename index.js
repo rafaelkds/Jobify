9a7b7858db1e993cfd334bc7f8688aa9e52e0a6d
Rafael <rafaelkds@gmail.com>
2019-05-19 14:00:00 -0300

    added port as env variable

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))//se a rota buscada não existir, tenta encontrar no public
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -88,7 +90,7 @@ const init = async () => {
 }
 init()
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if (err)
         console.log('Não foi possível iniciar o servidor')
     else
