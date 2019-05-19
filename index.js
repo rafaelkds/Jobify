46eb596af0f9e6d74bddca5af2028fec3b023d53
Rafael <rafaelkds@gmail.com>
2019-05-19 14:20:49 -0300

    fix path views and public

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))//se a rota buscada não existir, tenta encontrar no public
+app.use(express.static(path.join(__dirname, 'public')))//se a rota buscada não existir, tenta encontrar no public
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async (request, response) => {
